92b6cfeacad8aa8448b0d9f0861048da119da52a
40416085 <40416085@live.napier.ac.uk>
2020-01-24 13:48:22 +0000

    Fixed impressively enough, the mistake in "Hello World"

--- src/main/java/com/set08103/group14/Main.java
+++ src/main/java/com/set08103/group14/Main.java
@@ -2,7 +2,7 @@ package com.set08103.group14;
 
 public class Main {
 
-    public static void main() {
-        System.out.println("hello world");
+    public static void main(String[] args) {
+        System.out.println("hello, world");
     }
 }
