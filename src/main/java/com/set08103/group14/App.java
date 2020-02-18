d85c7fcf2c0c2027ce2de1e4ea70eea45588a893
CrazyCamie456 <camiecallaghan@gmail.com>
2020-02-18 10:24:33 +0000

    Setting up basic test code for later.

--- src/main/java/com/set08103/group14/App.java
+++ src/main/java/com/set08103/group14/App.java
@@ -3,6 +3,14 @@ package com.set08103.group14;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("hello, world");
+
+        System.out.println("Query 1");
+        Query1.run();
+
+        System.out.println("Query 2");
+        Query2.run();
+
+        System.out.println("Query 3");
+        Query3.run();
     }
 }
