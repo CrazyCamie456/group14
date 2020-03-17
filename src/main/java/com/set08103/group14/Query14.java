9286461e2bf3a79726ff0998b7d510561f23f224
ChefSemiColon <zachwalker17.zw@gmail.com>
2020-03-17 03:12:31 +0000

    Fixed error when making strings

--- src/main/java/com/set08103/group14/Query14.java
+++ src/main/java/com/set08103/group14/Query14.java
@@ -8,8 +8,8 @@ public class Query14 {
         DatabaseLink db = DatabaseLink.Instance();
         //defining variables
         ResultSet result;
-        String regionName = "";
-        String resultNumberText = "";
+        String regionName = new String("");
+        String resultNumberText = new String("");
         do{
             //Ask user for the number of cities they want
             System.out.println("Please enter the number of cities you want to see");
